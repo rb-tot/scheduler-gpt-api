e9156534c5b4425845fa4ff8876dc10f46d3366f
rbell <rbell@cgrs.com>
2025-08-24 17:20:42 -0600

    removed outdated files to archive outside of repository
    removed outdated files

--- test_fetch.py
+++
@@ -1,5 +0,0 @@
-from db_queries import job_pool_df, eligibility_df, technicians_df
-
-print("job_pool rows:", len(job_pool_df()))
-print("eligibility rows:", len(eligibility_df()))
-print("technicians rows:", len(technicians_df()))
--- test_supabase.py
+++
@@ -1,4 +0,0 @@
-from supabase_client import supabase
-
-res = supabase.table("technicians").select("technician_id,name").limit(1).execute()
-print(res.data)
